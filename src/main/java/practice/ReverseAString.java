f8dd8ccdc6469bc0a6827fc963cebeb5c3e5fdea
Navi <navitony5983@gmail.com>
2025-01-24 21:56:41 +0530

    Committing the java practice program 24-01-2025

---
+++ src/main/java/practice/ReverseAString.java
@@ -0,0 +1,38 @@
+package practice;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+
+        System.out.println(reverseUsingBuffer("Naveen"));
+        System.out.println(reverseUsingBuilder("Kumar"));
+        System.out.println(reverseString("Christy"));
+
+    }
+
+    public static String reverseUsingBuffer(String text){
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(text);
+        buffer.reverse();
+
+        return buffer.toString();
+    }
+
+    public static String reverseUsingBuilder(String text){
+        StringBuilder builder = new StringBuilder();
+        builder.append(text);
+        builder.reverse();
+
+        return builder.toString();
+    }
+
+    public static String reverseString(String text){
+        String reverse = "";
+        char[] charArray = text.toCharArray();
+        int count = charArray.length;
+        for(int i=count-1;i>=0;i--){
+            reverse = reverse+charArray[i];
+        }
+        return reverse;
+    }
+
+}
